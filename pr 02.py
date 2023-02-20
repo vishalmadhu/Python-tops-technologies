4e48481f951eec67a3fb24efc2fe372beff838ea
vishalmadhu <vishalmadhu143@gmail.com>
2023-02-20 22:28:10 +0530

    second commit

---
+++ pr 02.py
@@ -0,0 +1,14 @@
+a= '''vishal'''
+b= 17.50
+c= 254
+d = None
+print(a)
+print(b)
+print(c)
+print(d)
+
+# printing type of variables 
+print(type(a)) 
+print(type(b)) 
+print(type(c)) 
+print(type(d)) 
---
+++ pr chepter2.py
@@ -0,0 +1,23 @@
+a = 30
+b = 1
+print("the sum of a and b is", a + b)
+
+# pr2
+a = 458
+b = 15
+print("the remainder when a is divided by b is ", a%b)
+
+# pr5
+a = input("Enter first number:")
+b = input("Enter second number:")
+a=int(a)
+b=int(b)
+avg = (a+b)/2
+print("The average of a and b is ", avg)
+
+# pr6
+
+a = input("Enter your number:")
+a = int(a)
+print(a*a)
+
