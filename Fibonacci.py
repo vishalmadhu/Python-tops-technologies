147cce372b2216d9a3e90dbbcaaa3bae6a68ef2a
vishalmadhu <vishalmadhu143@gmail.com>
2023-02-23 20:25:35 +0530

    first commit

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,5 +1,9 @@
-n = int(input("Enter number: "))
-
-for i in range(x8 = x7 + x6):
-
-
+n=int(input("Enter the number :"))
+num1=0
+num2=1
+sum=0
+for i in range(n):
+    sum=num1+num2
+    print(sum)
+    num1=num2
+    num2=sum
