eb26d489266d5b02b1d4ca9e416e99cae0f4812f
vishalmadhu <vishalmadhu143@gmail.com>
2023-02-27 12:42:20 +0530

    first commit

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 n=int(input("Enter the number :"))
-=======
-num=int(input("Enter the number :"))
->>>>>>> 271d3e8b12ddc7ebc2f0cdde5d9e0a74ec772e05
 num1=0
 num2=1
 sum=0
@@ -10,8 +6,4 @@ for i in range(n):
     sum=num1+num2
     print(sum)
     num1=num2
-<<<<<<< HEAD
     num2=sum
-=======
-    num2=sum
->>>>>>> 271d3e8b12ddc7ebc2f0cdde5d9e0a74ec772e05
